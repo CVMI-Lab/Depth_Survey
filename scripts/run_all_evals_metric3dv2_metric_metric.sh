5f338003ef9f292bb3bbc4e499201d4bb76a6b9c
mxliu-hku <161350045+mx-liu6@users.noreply.github.com>
2025-08-08 19:40:09 +0800

    Add files via upload

---
+++ scripts/run_all_evals_metric3dv2_metric_metric.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+python eval.py configs/moge_benchmark/metric3dv2/metric3dv2-metric_DDAD.yaml
+python eval.py configs/moge_benchmark/metric3dv2/metric3dv2-metric_DIODE.yaml
+python eval.py configs/moge_benchmark/metric3dv2/metric3dv2-metric_ETH3D.yaml
+python eval.py configs/moge_benchmark/metric3dv2/metric3dv2-metric_GSO.yaml
+python eval.py configs/moge_benchmark/metric3dv2/metric3dv2-metric_HAMMER.yaml
+python eval.py configs/moge_benchmark/metric3dv2/metric3dv2-metric_iBims-1.yaml
+python eval.py configs/moge_benchmark/metric3dv2/metric3dv2-metric_KITTI.yaml
+python eval.py configs/moge_benchmark/metric3dv2/metric3dv2-metric_NYUv2.yaml
+python eval.py configs/moge_benchmark/metric3dv2/metric3dv2-metric_Sintel.yaml
+python eval.py configs/moge_benchmark/metric3dv2/metric3dv2-metric_Spring.yaml
